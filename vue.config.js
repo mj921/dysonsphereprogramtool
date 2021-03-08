885523a7dbaeb20904402586c3d28cddd8c4b955
majie <373002712@qq.com>
2021-03-08 11:05:37 +0800

    github:build

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,9 @@
+console.log(
+  process.env.NODE_ENV === "production" ? process.env.PUBLIC_PATH || "/" : "/",
+  process.env.PUBLIC_PATH
+);
 module.exports = {
   publicPath:
-    process.env.NODE_ENV === "production"
-      ? process.env.PUBLIC_PATH || "/"
-      : "/",
+    process.env.NODE_ENV === "production" ? process.env.PUBLIC_PATH : "/",
   outputDir: process.env.OUTPUT_DIR || "dist"
 };
